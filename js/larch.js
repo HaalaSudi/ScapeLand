cb0e78371af95a0eb8eeed93a022ad830478a733
HaalaSudi <58235781+HaalaSudi@users.noreply.github.com>
2020-09-30 20:25:32 +0100

    Add files via upload

---
+++ js/larch.js
@@ -0,0 +1,72 @@
+
+    // Width and Height of the side nav bar when opened
+function openNav() {
+  document.getElementById("sideBar").style.width = "250px";
+  document.getElementById("opnButton").style.marginLeft = "250px";
+}
+
+// Width and Height of the side nav bar when closed (should be set to 0)
+function closeNav() {
+  document.getElementById("sideBar").style.width = "0";
+  document.getElementById("opnButton").style.marginLeft = "0";
+}
+ 
+    
+//jQuery for the articles side bar    
+
+$(document).ready(function(){
+$(window).scroll(function () {   
+   
+ if($(window).scrollTop() > 200) {
+    $('#sideArticle').css('position','fixed');
+    $('#sideArticle').css('top','0'); 
+ }
+
+ else if ($(window).scrollTop() <= 200) {
+    $('#sideArticle').css('position','');
+    $('#sideArticle').css('top','');
+ }  
+    if ($('#sideArticle').offset().top + $("#sideArticle").height() > $("#image").offset().top) {
+        $('#sideArticle').css('top',-($("#sideArticle").offset().top + $("#sideArticle").height() - $("#image").offset().top));
+    }
+});
+});
+    
+    
+ // Get the elements with class="column"
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
